bec8010df7f7f3f92205470d8b6e62bd5eea5c92
icemiliang <abcd@abcd.com>
2021-09-24 17:51:12 -0700

    add more scripts to run

--- run_script.sh
+++ run_script.sh
@@ -4,8 +4,20 @@
 #  python run.py examples/bop_challenge/config_tless_random.yaml /mnt/LinuxDataFast/Datasets/bop_dataset tless /mnt/LinuxDataFast/Datasets/bop_toolkit resources/cctextures /mnt/LinuxDataFast/Datasets/bop_dataset
 #done
 
+## replicate some scenes and cameras and append normal maps only
+#for scene_id in {1..20}
+#do
+#  python run.py examples/bop_scene_replication/config_normal_only.yaml /media/hmiintern/8bd15be5-5b44-45d1-83ed-45049c3ccdbc/datasets/cosypose/local_data/bop_datasets tless /home/hmiintern/projects/bop_toolkit_cosypose /media/hmiintern/8bd15be5-5b44-45d1-83ed-45049c3ccdbc/datasets/blenderproc/bop_scene_replication test primesense $scene_id
+#done
+
 # replicate some scenes and cameras and append normal maps only
-for scene_id in {1..10}
+for scene_id in {1..30}
 do
-  python run.py examples/bop_scene_replication/config_normal_only.yaml /mnt/LinuxDataFast/Projects/cosypose/local_data/bop_datasets tless /mnt/LinuxDataFast/Projects/bop_toolkit_cosypose examples/bop_scene_replication/output $scene_id
+  python run.py examples/bop_scene_replication/config_normal_only.yaml /media/hmiintern/8bd15be5-5b44-45d1-83ed-45049c3ccdbc/datasets/cosypose/local_data/bop_datasets tless /home/hmiintern/projects/bop_toolkit_cosypose /media/hmiintern/8bd15be5-5b44-45d1-83ed-45049c3ccdbc/datasets/blenderproc/bop_scene_replication train primesense $scene_id
 done
+
+## replicate some scenes and cameras and append normal maps only
+#for scene_id in {0..49}
+#do
+#  python run.py examples/bop_scene_replication/config_normal_only.yaml /media/hmiintern/8bd15be5-5b44-45d1-83ed-45049c3ccdbc/datasets/cosypose/local_data/bop_datasets tless /home/hmiintern/projects/bop_toolkit_cosypose /media/hmiintern/8bd15be5-5b44-45d1-83ed-45049c3ccdbc/datasets/blenderproc/bop_scene_replication train pbr $scene_id
+#done
